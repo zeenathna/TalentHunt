3d83f9c0a76c68e3662cdaa591a3748bb1dacbb0
zeenathna <60476593+zeenathna@users.noreply.github.com>
2024-10-19 15:53:22 -0700

    Create emailTemplate.js

---
+++ backend/config/emailTemplate.js
@@ -0,0 +1,7 @@
+const emailTemplate = (firstName) => `
+  <h1>Welcome to TalentHunt, ${firstName}!</h1>
+  <p>Thank you for signing up. We're excited to have you on board!</p>
+  <p>Best regards,<br />The TalentHunt Team</p>
+`;
+
+module.exports = emailTemplate;
